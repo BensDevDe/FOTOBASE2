934f30e0b467e29e98c4f3473cb5536ca32e6a27
stasiks1001 <stanislav_ivanov31@abv.bg>
2022-03-09 10:04:08 +0100

    second commit

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(cors());
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 .then( ()=>{
-    console.log(`Database connected :) `.underline.green.bold);
+    console.log(`Database connected :)`);
 })
 .catch(err =>{
     console.log(err);
 });
 
 app.listen(3000, () => {
-    console.log(`server started on port 4000 :) `.underline.yellow.bold);
+    console.log(`server started on port 3000 :) `);
 });
